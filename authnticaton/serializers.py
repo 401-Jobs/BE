31e33f7ea6e79bdc5e1a6de430cf82faf4cfa6a9
alaa-adoos <alaaabualadoos@gmail.com>
2023-01-16 09:53:00 +0000

    authntication

---
+++ authnticaton/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
+class RegisterSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(max_length=68,min_length=6,write_only=True)
+    
+    class Meta:
+        model=User
+        fields=['email','username','password']
+    def validate(self, attrs):
+        email=attrs.get('email', '')
+        username=attrs.get('username', '')
+        if not username.isalnum():
+            raise serializers.ValidationError('usre name should only have a char')
+        return attrs
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+class EmailVerification(serializers.ModelSerializer):
+    token=serializers.CharField(max_length=555)
+
+    class Meta:
+        model=User
+        fields=['token']
+
+class LoginSerializer(serializers.ModelSerializer[User]):
+    email=serializers.EmailField(max_length=255,min_length=3)
+    password=serializers.CharField(max_length=68,min_length=6,write_only=True)
+    username=serializers.CharField(max_length=255,min_length=3,read_only=True)
+    tokens=serializers.CharField(max_length=68,min_length=6,read_only=True)
+
+    class Meta:
+        model=User
+        fields=['email','password','username','tokens']
+    def validate(self, attrs):
+        email=attrs.get('email', '')
+        password=attrs.get('password', '')
+        print(email,password)
+        user=auth.authenticate(email=email,password=password)
+       
+        if  user is None:
+            raise serializers.ValidationError('A user with this email and password was not found.')   
+        if not user.is_active:
+            raise serializers.ValidationError('Account disabled')
+        if not user.is_verified:
+            raise serializers.ValidationError('Email is not vaerified')  
+
+        return {
+            'email':user.email,
+            'username':user.username,
+            'tokens':user.tokens
+         }     
+
+       
+
+        return super().validate(attrs)
