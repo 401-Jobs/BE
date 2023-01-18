8bdca817593bc199629ba6b2fbcc38593a8e9667
Abdelrahman Alasal <AbdelrahmanAlasal@gmail.com>
2023-01-18 14:18:39 +0300

    api

--- account/models.py
+++ account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from rest_framework_simplejwt.tokens import RefreshToken
 # Create your models here.
+from django.contrib.auth.hashers import make_password
 
 
 class CustomUser(AbstractUser):
@@ -26,6 +27,11 @@ class CustomUser(AbstractUser):
         }
     def __str__(self) -> str:
          return self.username
+
+    def save(self, *args, **kwargs):
+        if self.password:
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
 
 
 class JobSeeker(models.Model):
